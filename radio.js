97983e69600af5df54d89470f807c80e211072e8
TalineRodrigues <141073690+TalineRodrigues@users.noreply.github.com>
2023-09-25 14:17:27 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -3,7 +3,7 @@ let musicas = [
 
     {titulo:'ARE WE STILL FRIENDS', artista:'The Creator', src:'ARE WE STILL FRIENDS__Gb76TgCUqAY.mp3', img:'1.png'},
 
-    {titulo:'POTATO SALAD', artista:'Tyler, The Creator', src:'A_AP ROCKY X TYLER THE CREATOR - POTATO SALAD_93M1QtYDtpU.mp3', img:'5.PNG'},
+    {titulo:'POTATO SALAD', artista:'Tyler, The Creator', src:'A_AP ROCKY X TYLER THE CREATOR - POTATO SALAD_93M1QtYDtpU.mp3', img:'5.png'},
 
     {titulo:'BEST INTEREST', artista:'Tyler, The Creator', src:'BEST INTEREST_NkMTKGM-efw.mp3', img:'radio/2.png'},
 
